a641feb4d1205a989911cbf6d02db4495e708ea7
EskimoA000 <45653177+EskimoA000@users.noreply.github.com>
2019-05-17 20:41:43 +0800

    Update objects.h

--- share/objects.h
+++ share/objects.h
@@ -68,6 +68,49 @@ struct Billing
   double startTemp;
   double endTemp;
   double rate;
+  int action;
+  
+  Billing(int _billingId=0, int _roomId=0, QString _userId="haha", QDateTime _start=QDateTime(),
+  		QDateTime _duration=QDateTime(), double _costs=0, double _wdspd=0, double _startTemp=0,
+		double _endTemp=0, double _rate=0, int _action=0):
+  	billingId(_billingId), roomId(_roomId), userId(_userId), start(_start), duration(_duration),
+		costs(_costs), wdspd(_wdspd), startTemp(_startTemp), endTemp(_endTemp), rate(_rate), acton(_action)
+	{}
+};
+struct Manager{
+	QString manId;
+	QString manpwsd;
+	int privilege;
+	
+	Manager(QString _manId="yy", QString _manpwsd="xx", int _privilege=0):
+		manId(_manId), manpwsd(_manpwsd), privilege(_privilege)
+	{} 
+};
+struct Host{
+	int hostId;
+	int startTemp;
+	int mode;
+	double paraLow;
+	double paraMid;
+	double paraHigh;
+	double lowRate;
+	double midRate;
+	double highRate;
+	int maxTemp;
+	int minTemp;
+	int defaultTemp;
+	int defaultWdspd;
+	int modeSche;
+	int state;
+	
+	Host(int _hostId=0, int _startTemp=0, int _mode=0, double paraLow=0, double _paraMid=0, 
+			double _paraHigh=0, double _lowRate=0, double _midRate=0, double _highRate=0, int _maxTemp=0,
+			int _minTemp=0, int _defaultTemp=0, int _defaultWdspd=0, int _modeSche=0, int _state=0):
+		hostId(_hostId), startTemp(_startTemp), mode(_mode), paraLow(_paraLow), paraMid(_paraMid),
+		paraHigh(_paraHigh), lowRate(_lowRate), midRate(_midRate), highRate(_highRate), maxTemp(_maxTemp),
+		minTemp(_minTemp), defaultTemp(_defaultTemp), defaultWdspd(_defaultWdspd), modeSche(_modeSche),
+		state(_state)
+		{}	
 };
 
 #endif // OBJECTS_H
