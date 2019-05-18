098e8abb49f1371e59056cbb5740f3dab2878ad6
dyzsr <dybest@icloud.com>
2019-05-18 22:06:33 +0800

    update reception

--- client/reception.cpp
+++ client/reception.cpp
@@ -1,11 +1,40 @@
 #include "reception.h"
 
-Reception::Reception(QObject *parent) : QObject(parent)
+Reception::Reception(QObject *parent, Pipe *_pipe):
+  QObject(parent),
+  pipe(_pipe)
 {
+  Q_ASSERT(pipe != nullptr);
+}
 
+void Reception::setRoom(const Room &_room)
+{
+  room = _room;
+}
+
+QVector<Billing> Reception::getBillings(QDateTime start, QDateTime end)
+{
+  int front = 0, rear = billings.size();
+  if (start.isValid()) {
+    for (const Billing &billing: billings) {
+      if (start < billing.start)
+        break;
+      front++;
+    }
+  }
+  if (end.isValid()) {
+    for (auto it_billing = billings.rbegin();
+         it_billing != billings.rend();
+         ++it_billing) {
+      if (it_billing->start < end)
+        break;
+      rear--;
+    }
+  }
+  return billings.mid(front, rear - front);
 }
 
 void Reception::fetchBillings()
 {
-
+  pipe->getBillings(room.usrId, room.roomId);
 }
