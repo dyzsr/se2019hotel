9d4834c4ca70c46aac7fc428f4f57a3b4275659f
dyzsr <dybest@icloud.com>
2019-05-22 17:33:42 +0800

    update UI

--- server/mainwindow.h
+++ server/mainwindow.h
@@ -15,6 +15,11 @@ public:
   explicit MainWindow(QWidget *parent = nullptr);
   ~MainWindow();
 
+private slots:
+  void on_bt_signIn_clicked();
+
+  void on_bt_back_clicked();
+
 private:
   Ui::MainWindow *ui;
 };
