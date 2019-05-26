af3cb752bab8c55981519594de44d3ff264bd98b
Disciple-G <37109610+Disciple-G@users.noreply.github.com>
2019-05-26 20:51:27 +0800

    房间信息能看了

--- server/manager.h
+++ server/manager.h
@@ -13,26 +13,21 @@ class Manager : public QObject
 public:
   explicit Manager(QObject *parent = nullptr);
   ~Manager();
+    QVector<QString> getRoomStateStr();
+    Room getOneRoom(int roomId);
 
 signals:
-  QVector<Billing> sgn_getBilling(QDateTime, QDateTime);
 
 public slots:
   bool signIn(QString name, QString pswd);
   bool signOut();
 
-  QVector<Billing> getDayReport();
-  QVector<Billing> getWeekReport();
-  QVector<Billing> getMonthReport();
-  QVector<Billing> getReport(QDateTime start, QDateTime end);
-
 private:
   Pipe *pipe;
 
   Admin adm;
-  QVector<Billing> billings;
-
-  void fetchBillings(QDateTime start, QDateTime end);
+  QVector<Room> getAllRooms();
+  QString stateToStr(int state);
 };
 
 #endif // MANAGER_H
