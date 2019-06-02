481a73dc16c98882b12fc1d3c6048286efcb0840
Disciple-G <37109610+Disciple-G@users.noreply.github.com>
2019-06-02 18:23:29 +0800

    getIn_Removed

--- client/mainwindow.h
+++ client/mainwindow.h
@@ -33,7 +33,6 @@ public slots:
 private slots:
   void on_bt_signIn_clicked();
   void on_bt_signOut_clicked();
-  void on_bt_getIn_clicked();
   void on_bt_tempUp_clicked();
   void on_bt_tempDown_clicked();
   void on_sb_settemp_valueChanged(int arg1);
